d066e9361eddcd0ed2922d7839c05be88f3ac274
Alexander QA <112622680+qartavstev-qameta@users.noreply.github.com>
2025-04-08 21:20:05 +0300

    Update sample.js

--- features/support/sample.js
+++ features/support/sample.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from "@cucumber/cucumber";
+import { Given, When, Then, And } from "@cucumber/cucumber";
 import { step } from "allure-js-commons";
 
 // === GIVEN ===
@@ -15,7 +15,7 @@ Given("the user is logged in", async function () {
   });
 });
 
-Given("the session timeout duration has elapsed (e.g., 15 minutes of inactivity)", async function () {
+And("the session timeout duration has elapsed (e.g., 15 minutes of inactivity)", async function () {
   await step("Simulating session timeout", async () => {
     console.log("Session has timed out");
   });
@@ -30,7 +30,7 @@ When("the user enters an invalid username or password", async function () {
   console.log("Entered invalid credentials");
 });
 
-When("the user clicks the Login button", async function () {
+And("clicks the Login button", async function () {
   console.log("Clicked Login button");
 });
 
@@ -59,7 +59,7 @@ Then("the user should be redirected to the dashboard page", async function () {
   console.log("Redirected to dashboard");
 });
 
-Then("a success message should be displayed, saying Login successful", async function () {
+And("a success message should be displayed, saying Login successful", async function () {
   console.log("Login successful message shown");
 });
 
@@ -67,7 +67,7 @@ Then("an error message should be displayed, saying Invalid username or password"
   console.log("Invalid credentials message shown");
 });
 
-Then("the user should remain on the login page", async function () {
+And("the user should remain on the login page", async function () {
   console.log("Still on login page");
 });
 
@@ -79,7 +79,7 @@ Then("the password should be masked (e.g., displayed as asterisks or dots)", asy
   console.log("Password is masked");
 });
 
-Then("the user should be able to toggle password visibility (if supported)", async function () {
+And("the user should be able to toggle password visibility (if supported)", async function () {
   console.log("Toggle visibility works");
 });
 
@@ -87,7 +87,7 @@ Then("the account should be locked", async function () {
   console.log("Account is locked");
 });
 
-Then("a message should be displayed, saying Your account is locked. Please contact support", async function () {
+And("a message should be displayed, saying Your account is locked. Please contact support", async function () {
   console.log("Account locked message shown");
 });
 
@@ -95,14 +95,14 @@ Then("the user should be redirected to the login page", async function () {
   console.log("Redirected to login");
 });
 
-Then("a message should be displayed, saying You have successfully logged out", async function () {
+And("a message should be displayed, saying You have successfully logged out", async function () {
   console.log("Logout message shown");
 });
 
-Then("the session should be terminated", async function () {
+And("the session should be terminated", async function () {
   console.log("Session terminated");
 });
 
-Then("a message should be displayed, saying Your session has expired. Please log in again", async function () {
+And("a message should be displayed, saying Your session has expired. Please log in again", async function () {
   console.log("Session expired message shown");
 });
