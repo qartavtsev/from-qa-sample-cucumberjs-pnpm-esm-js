d6a5c733ff9e91a4d50153f42758873840953c1e
qartavtsev <qartavtsev@gmail.com>
2025-06-26 20:52:37 +0300

    Update basic_group.js

--- features/support/basic_group.js
+++ features/support/basic_group.js
@@ -1,1 +1,15 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { step } from "allure-js-commons";
 
+// === GIVEN ===
+Given(`user is logged in with user role "Publisher Super Admin"`, async function () {
+  await step("action", async () => {
+    console.log("action");
+  });
+});
+
+// === WHEN ===
+When('user performs "delete" a "basic group" operation with parameters {string} and {string}', async function (identityDescription, identityCode) {
+      await allure.parameter("identityDescription", identityDescription);
+      await allure.parameter("identityCode", identityCode);
+});
