1bab980318b659205e293f03303a98c7502c63a0
qartavtsev <qartavtsev@gmail.com>
2025-06-26 21:08:21 +0300

    Update basic_group.js

--- features/support/basic_group.js
+++ features/support/basic_group.js
@@ -1,6 +1,5 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { allure } from 'allure-cucumberjs';
-import { step } from "allure-js-commons";
+import { allure, step } from "allure-js-commons";
 
 // === GIVEN ===
 Given(`user is logged in with user role "Publisher Super Admin"`, async function () {
